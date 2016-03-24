a3ee0c261080bb924c0aec0211279f090d147f7e
Clang <clangcn@gmail.com>
2016-03-24 15:45:59 +0800

    更新随机密码计算方式及脚本优化

--- install-game-server.sh
+++ install-game-server.sh
@@ -7,7 +7,7 @@ export PATH
 #   Author: Clang <admin@clangcn.com>
 #   Intro:  http://clang.cn
 #===============================================================================================
-version="3.0"
+version="4.0"
 str_game_dir="/usr/local/game-server"
 # Check if user is root
 function rootness(){
@@ -29,10 +29,10 @@ function get_char(){
 function fun_clang.cn(){
     echo ""
     echo "####################################################################"
-    echo "# Install Game-Server(XiaoBao) for CentOS Debian or Ubuntu(32/64bit)"
-    echo "# Intro: http://clang.cn"
-    echo "# Author: Clang <admin@clangcn.com>"
-    echo "# Version ${version}"
+    echo " Install Game-Server(XiaoBao) for CentOS Debian or Ubuntu(32/64bit)"
+    echo " Intro: http://clang.cn"
+    echo " Author: Clang <admin@clangcn.com>"
+    echo " Version ${version}"
     echo "####################################################################"
     echo ""
 }
@@ -57,7 +57,7 @@ function getversion(){
         grep -oE  "[0-9.]+" /etc/redhat-release
     else    
         grep -oE  "[0-9.]+" /etc/issue
-    fi    
+    fi
 }
 
 # CentOS version
@@ -69,7 +69,7 @@ function centosversion(){
         return 0
     else
         return 1
-    fi        
+    fi
 }
 
 # Check OS bit
@@ -104,7 +104,8 @@ function fun_check_port(){
         if [ -n "${checkServerPort}" ]; then
             echo ""
             echo -e "\033[31m\033[01mError:\033[0m Port \033[32m${strServerPort}\033[0m is \033[35m\033[01mused\033[0m,view relevant port:"
-            netstat -apn | grep "\b:${strServerPort}\b"
+            #netstat -apn | grep "\b:${strServerPort}\b"
+            netstat -ntulp | grep "\b:${strServerPort}\b"
             fun_input_port
         else
             serverport="${strServerPort}"
@@ -125,86 +126,91 @@ function fun_input_port(){
     fun_check_port "${serverport}"
 }
 
+# Random password
+function fun_randstr(){
+  index=0
+  strRandomPass=""
+  for i in {a..z}; do arr[index]=$i; index=`expr ${index} + 1`; done
+  for i in {A..Z}; do arr[index]=$i; index=`expr ${index} + 1`; done
+  for i in {0..9}; do arr[index]=$i; index=`expr ${index} + 1`; done
+  for i in {1..16}; do strRandomPass="$strRandomPass${arr[$RANDOM%$index]}"; done
+  echo $strRandomPass
+}
+
 function pre_install_clang(){
-# install
-echo "==============Install packs=============="
-if [ "${OS}" == 'CentOS' ]; then
-    #yum -y update
-    yum -y install nano net-tools openssl-devel wget iptables policycoreutils curl curl-devel psmisc
-else
-    apt-get update -y
-    apt-get install -y wget nano screen openssl libcurl4-openssl-dev iptables curl psmisc
-fi
-clear
-#config setting
-echo "#################################################################"
-echo "#"
-echo "# Please input your Game-Server(XiaoBao) server_port and password"
-echo "#"
-echo "#################################################################"
-echo ""
-sshport=`netstat -anp |grep ssh | grep '0.0.0.0:'|cut -d: -f2| awk 'NR==1 { print $1}'`
-defIP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.' | cut -d: -f2 | awk 'NR==1 { print $1}'`
-if [ "${defIP}" = "" ]; then
-    defIP=$(curl -s -4 icanhazip.com)
-fi
-IP="0.0.0.0"
-echo "Please input VPS IP"
-read -p "(You VPS IP:$defIP, Default IP: $IP):" IP
-if [ "$IP" = "" ]; then
+    #config setting
+    echo " Please input your Game-Server(XiaoBao) server_port and password"
+    echo ""
+    sshport=`netstat -anp |grep ssh | grep '0.0.0.0:'|cut -d: -f2| awk 'NR==1 { print $1}'`
+    defIP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.' | cut -d: -f2 | awk 'NR==1 { print $1}'`
+    if [ "${defIP}" = "" ]; then
+        defIP=$(curl -s -4 icanhazip.com)
+    fi
     IP="0.0.0.0"
-fi
-
-fun_input_port
-echo ""
-shadowsocks_pwd=`openssl rand -base64 12`
-read -p "Please input Password (Default Password: ${shadowsocks_pwd}):" shadowsockspwd
-if [ "${shadowsockspwd}" = "" ]; then
-    shadowsockspwd="${shadowsocks_pwd}"
-fi
-echo ""
-ssmethod="chacha20"
-echo "Please input Encryption method(chacha20, aes-256-cfb, bf-cfb, des-cfb, rc4)"
-read -p "(Default method: ${ssmethod}):" ssmethod
-if [ "${ssmethod}" = "" ]; then
+    echo "Please input VPS IP"
+    read -p "(You VPS IP:$defIP, Default IP: $IP):" IP
+    if [ "$IP" = "" ]; then
+        IP="0.0.0.0"
+    fi
+    fun_input_port
+    echo ""
+    shadowsocks_pwd=`fun_randstr`
+    read -p "Please input Password (Default Password: ${shadowsocks_pwd}):" shadowsockspwd
+    if [ "${shadowsockspwd}" = "" ]; then
+        shadowsockspwd="${shadowsocks_pwd}"
+    fi
+    echo ""
     ssmethod="chacha20"
-fi
-echo ""
-set_iptables="n"
-    echo  -e "\033[33mDo you want to set iptables?\033[0m"
-    read -p "(if you want please input: y,Default [no]):" set_iptables
-
-    case "${set_iptables}" in
-    y|Y|Yes|YES|yes|yES|yEs|YeS|yeS)
-    echo "You will set iptables!"
-    set_iptables="y"
-    ;;
-    n|N|No|NO|no|nO)
-    echo "You will NOT set iptables!"
+    echo "Please input Encryption method(chacha20, aes-256-cfb, bf-cfb, des-cfb, rc4)"
+    read -p "(Default method: ${ssmethod}):" ssmethod
+    if [ "${ssmethod}" = "" ]; then
+        ssmethod="chacha20"
+    fi
+    echo ""
     set_iptables="n"
-    ;;
-    *)
-    echo "The iptables is not set!"
-    set_iptables="n"
-    esac
+        echo  -e "\033[33mDo you want to set iptables?\033[0m"
+        read -p "(if you want please input: y,Default [no]):" set_iptables
 
-echo ""
-echo "============== Check your input =============="
-echo -e "Your Server IP:\033[32m\033[01m${defIP}\033[0m"
-echo -e "Your Set IP:\033[32m\033[01m${IP}\033[0m"
-echo -e "Your Server Port:\033[32m\033[01m${serverport}\033[0m"
-echo -e "Your Password:\033[32m\033[01m${shadowsockspwd}\033[0m"
-echo -e "Your Encryption Method:\033[32m\033[01m${ssmethod}\033[0m"
-echo -e "Your SSH Port:\033[32m \033[01m${sshport}\033[0m"
-echo "=============================================="
-echo ""
-echo "Press any key to start...or Press Ctrl+c to cancel"
+        case "${set_iptables}" in
+        y|Y|Yes|YES|yes|yES|yEs|YeS|yeS)
+        echo "You will set iptables!"
+        set_iptables="y"
+        ;;
+        n|N|No|NO|no|nO)
+        echo "You will NOT set iptables!"
+        set_iptables="n"
+        ;;
+        *)
+        echo "The iptables is not set!"
+        set_iptables="n"
+        esac
 
-char=`get_char`
-[ ! -d ${str_game_dir} ] && mkdir -p ${str_game_dir}
-cd ${str_game_dir}
-echo $PWD
+    echo ""
+    echo "============== Check your input =============="
+    echo -e "Your Server IP:\033[32m\033[01m${defIP}\033[0m"
+    echo -e "Your Set IP:\033[32m\033[01m${IP}\033[0m"
+    echo -e "Your Server Port:\033[32m\033[01m${serverport}\033[0m"
+    echo -e "Your Password:\033[32m\033[01m${shadowsockspwd}\033[0m"
+    echo -e "Your Encryption Method:\033[32m\033[01m${ssmethod}\033[0m"
+    echo -e "Your SSH Port:\033[32m \033[01m${sshport}\033[0m"
+    echo "=============================================="
+    echo ""
+    echo "Press any key to start...or Press Ctrl+c to cancel"
 
+    char=`get_char`
+
+    echo "============== Install packs =============="
+    if [ "${OS}" == 'CentOS' ]; then
+        #yum -y update
+        yum -y install nano net-tools openssl-devel wget iptables policycoreutils curl curl-devel psmisc
+    else
+        apt-get update -y
+        apt-get install -y wget nano screen openssl libcurl4-openssl-dev iptables curl psmisc
+    fi
+
+    [ ! -d ${str_game_dir} ] && mkdir -p ${str_game_dir}
+    cd ${str_game_dir}
+    echo $PWD
 
 # Config shadowsocks
 cat > ${str_game_dir}/config.json<<-EOF
@@ -217,67 +223,68 @@ cat > ${str_game_dir}/config.json<<-EOF
     "method":"${ssmethod}"
 }
 EOF
-chmod 400 ${str_game_dir}/config.json
-rm -f ${str_game_dir}/game-server
-if [ "${Is_64bit}" == 'y' ] ; then
-    if [ ! -s ${str_game_dir}/game-server ]; then
-        if ! wget --no-check-certificate https://github.com/clangcn/game-server/raw/master/game-server -O ${str_game_dir}/game-server; then
-            echo "Failed to download game-server file!"
-            exit 1
+    chmod 400 ${str_game_dir}/config.json
+    rm -f ${str_game_dir}/game-server
+    if [ "${Is_64bit}" == 'y' ] ; then
+        if [ ! -s ${str_game_dir}/game-server ]; then
+            if ! wget --no-check-certificate https://github.com/clangcn/game-server/raw/master/game-server -O ${str_game_dir}/game-server; then
+                echo "Failed to download game-server file!"
+                exit 1
+            fi
         fi
-    fi
-else
-     if [ ! -s ${str_game_dir}/game-server ]; then
-        if ! wget --no-check-certificate https://github.com/clangcn/game-server/raw/master/game-server-x86 -O ${str_game_dir}/game-server; then
-            echo "Failed to download game-server file!"
-            exit 1
-        fi
-    fi
-fi
-[ ! -x ${str_game_dir}/game-server ] && chmod 755 ${str_game_dir}/game-server
-if [ "${OS}" == 'CentOS' ]; then
-    if [ ! -s /etc/init.d/game-server ]; then
-        if ! wget --no-check-certificate https://github.com/clangcn/game-server/raw/master/init/centos-game-server.init -O /etc/init.d/game-server; then
-            echo "Failed to download game-server.init file!"
-            exit 1
-        fi
-    fi
-    chmod +x /etc/init.d/game-server
-    chkconfig --add game-server
-else
-    if [ ! -s /etc/init.d/game-server ]; then
-        if ! wget --no-check-certificate https://github.com/clangcn/game-server/raw/master/init/debian-game-server.init -O /etc/init.d/game-server; then
-            echo "Failed to download game-server.init file!"
-            exit 1
-        fi
-    fi
-    chmod +x /etc/init.d/game-server
-    update-rc.d -f game-server defaults
-fi
-
-if [ "$set_iptables" == 'y' ]; then
-    # iptables config
-    iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-    iptables -I INPUT -p tcp --dport ${serverport} -j ACCEPT
-    iptables -I INPUT -p udp --dport ${serverport} -j ACCEPT
-    if [ "${OS}" == 'CentOS' ]; then
-        service iptables save
     else
-        echo '#!/bin/bash' > /etc/network/if-post-down.d/iptables
-        echo 'iptables-save > /etc/iptables.rules' >> /etc/network/if-post-down.d/iptables
-        echo 'exit 0;' >> /etc/network/if-post-down.d/iptables
-        chmod +x /etc/network/if-post-down.d/iptables
-
-        echo '#!/bin/bash' > /etc/network/if-pre-up.d/iptables
-        echo 'iptables-restore < /etc/iptables.rules' >> /etc/network/if-pre-up.d/iptables
-        echo 'exit 0;' >> /etc/network/if-pre-up.d/iptables
-        chmod +x /etc/network/if-pre-up.d/iptables
+         if [ ! -s ${str_game_dir}/game-server ]; then
+            if ! wget --no-check-certificate https://github.com/clangcn/game-server/raw/master/game-server-x86 -O ${str_game_dir}/game-server; then
+                echo "Failed to download game-server file!"
+                exit 1
+            fi
+        fi
     fi
-fi
-ln -s ${str_game_dir}/game-server /usr/bin/
-/etc/init.d/game-server start
-fun_clang.cn
-#install successfully
+    [ ! -x ${str_game_dir}/game-server ] && chmod 755 ${str_game_dir}/game-server
+    if [ "${OS}" == 'CentOS' ]; then
+        if [ ! -s /etc/init.d/game-server ]; then
+            if ! wget --no-check-certificate https://github.com/clangcn/game-server/raw/master/init/centos-game-server.init -O /etc/init.d/game-server; then
+                echo "Failed to download game-server.init file!"
+                exit 1
+            fi
+        fi
+        chmod +x /etc/init.d/game-server
+        chkconfig --add game-server
+    else
+        if [ ! -s /etc/init.d/game-server ]; then
+            if ! wget --no-check-certificate https://github.com/clangcn/game-server/raw/master/init/debian-game-server.init -O /etc/init.d/game-server; then
+                echo "Failed to download game-server.init file!"
+                exit 1
+            fi
+        fi
+        chmod +x /etc/init.d/game-server
+        update-rc.d -f game-server defaults
+    fi
+
+    if [ "$set_iptables" == 'y' ]; then
+        # iptables config
+        iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+        iptables -I INPUT -p tcp --dport ${serverport} -j ACCEPT
+        iptables -I INPUT -p udp --dport ${serverport} -j ACCEPT
+        if [ "${OS}" == 'CentOS' ]; then
+            service iptables save
+        else
+            echo '#!/bin/bash' > /etc/network/if-post-down.d/iptables
+            echo 'iptables-save > /etc/iptables.rules' >> /etc/network/if-post-down.d/iptables
+            echo 'exit 0;' >> /etc/network/if-post-down.d/iptables
+            chmod +x /etc/network/if-post-down.d/iptables
+
+            echo '#!/bin/bash' > /etc/network/if-pre-up.d/iptables
+            echo 'iptables-restore < /etc/iptables.rules' >> /etc/network/if-pre-up.d/iptables
+            echo 'exit 0;' >> /etc/network/if-pre-up.d/iptables
+            chmod +x /etc/network/if-pre-up.d/iptables
+        fi
+    fi
+    ln -s ${str_game_dir}/game-server /usr/bin/
+    /etc/init.d/game-server start
+    echo ""
+    fun_clang.cn
+    #install successfully
     echo ""
     echo "Congratulations, Game-Server(XiaoBao) install completed!"
     echo -e "Your Server IP:\033[32m\033[01m${defIP}\033[0m"
@@ -290,8 +297,9 @@ fun_clang.cn
     echo -e "Game-Server(XiaoBao) status manage: \033[45;37m/etc/init.d/game-server\033[0m {\033[40;35mstart\033[0m|\033[40;32mstop\033[0m|\033[40;33mrestart\033[0m|\033[40;36mstatus\033[0m}"
     #iptables -L -n
 }
-############################### install function##################################
+############################### install function ##################################
 function install_game_server_clang(){
+    fun_clang.cn
     checkos
     check_centosversion
     check_os_bit
@@ -302,7 +310,7 @@ function install_game_server_clang(){
         pre_install_clang
     fi
 }
-############################### configure function##################################
+############################### configure function ##################################
 function configure_game_server_clang(){
     if [ -s ${str_game_dir}/config.json ]; then
         nano ${str_game_dir}/config.json
@@ -310,10 +318,11 @@ function configure_game_server_clang(){
         echo "Game-Server(XiaoBao) configuration file not found!"
     fi
 }
-############################### uninstall function##################################
+############################### uninstall function ##################################
 function uninstall_game_server_clang(){
-     if [ -s /etc/init.d/game-server ] || [ -s ${str_game_dir}/game-server ] ; then
-        echo "==============Uninstall Game-Server(XiaoBao)=============="
+    fun_clang.cn
+    if [ -s /etc/init.d/game-server ] || [ -s ${str_game_dir}/game-server ] ; then
+        echo "============== Uninstall Game-Server(XiaoBao) =============="
         save_config="n"
         echo  -e "\033[33mDo you want to keep the configuration file?\033[0m"
         read -p "(if you want please input: y,Default [no]):" save_config
@@ -341,7 +350,7 @@ function uninstall_game_server_clang(){
         else
             update-rc.d -f game-server remove
         fi
-        rm -f /usr/bin/game-server /etc/init.d/game-server /var/run/game-server.pid
+        rm -f /usr/bin/game-server /etc/init.d/game-server /var/run/game-server.pid /root/game-server-install.log /root/game-server-update.log
         if [ "${save_config}" == 'n' ]; then
             rm -fr ${str_game_dir}
         else
@@ -353,10 +362,11 @@ function uninstall_game_server_clang(){
     fi
     echo ""
 }
-############################### update function##################################
+############################### update function ##################################
 function update_game_server_clang(){
+    fun_clang.cn
     if [ -s /etc/init.d/game-server ] || [ -s ${str_game_dir}/game-server ] ; then
-        echo "==============Update Game-Server(XiaoBao)=============="
+        echo "============== Update Game-Server(XiaoBao) =============="
         checkos
         check_centosversion
         check_os_bit
@@ -412,26 +422,26 @@ function update_game_server_clang(){
 }
 
 clear
-fun_clang.cn
 rootness
 # Initialization
 action=$1
 [  -z $1 ]
 case "$action" in
 install)
-    install_game_server_clang
+    install_game_server_clang 2>&1 | tee /root/game-server-install.log
     ;;
 config)
     configure_game_server_clang
     ;;
 uninstall)
-    uninstall_game_server_clang
+    uninstall_game_server_clang 2>&1 | tee /root/game-server-uninstall.log
     ;;
 update)
-    update_game_server_clang
+    update_game_server_clang 2>&1 | tee /root/game-server-update.log
     ;;
 *)
+    fun_clang.cn
     echo "Arguments error! [${action} ]"
-    echo "Usage: `basename $0` {install|uninstall|update|configure}"
+    echo "Usage: `basename $0` {install|uninstall|update|config}"
     ;;
 esac
